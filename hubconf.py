8e815ad3f93b29f8705b9e69ea1726dbefd43684
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-08-15 12:08:33 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,9 +1,9 @@
-dependencies = ["numpy", "torch", "torchaudio", "util"]
+dependencies = ["numpy", "torch", "torchaudio", ".util"]
 
 import torch
 import os
 import numpy as np
-from util.model_wrapper import ModelWrapper
+from .util.model_wrapper import ModelWrapper
 
 def fast_vad():
     """
