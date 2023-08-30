b147c04df06a0967725cf63ebd11b3ce2048dc12
Oliver Guhr <oliver.guhr@posteo.de>
2023-08-30 08:30:39 +0200

    added option to switch model

--- hubconf.py
+++ hubconf.py
@@ -12,3 +12,11 @@ def fast_vad():
     dirname    = os.path.dirname(__file__)
     model_path = os.path.join(dirname, "pretrained", "dnn_dnn_gru_spcen_512_256.jit")
     return DNNDNNGRUModelWrapper(model_path)
+
+def fast_vad_dnn():
+    """
+    returns a classifier
+    """
+    dirname    = os.path.dirname(__file__)
+    model_path = os.path.join(dirname, "pretrained", "dnn_dnn_spcen_512_256.jit")
+    return DNNDNNModelWrapper(model_path)
