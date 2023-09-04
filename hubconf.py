e7accac233745b19d919a48d085b9dd8a3d6a9ad
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-09-04 11:00:59 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -10,5 +10,13 @@ def fast_vad():
     returns a classifier
     """
     dirname    = os.path.dirname(__file__)
+    model_path = os.path.join(dirname, "pretrained", "dnn_dnn_gru_spcen_512_256.jit")
+    return DNNDNNGRUModelWrapper(model_path)
+
+def fast_vad_dnn():
+    """
+    returns a classifier
+    """
+    dirname    = os.path.dirname(__file__)
     model_path = os.path.join(dirname, "pretrained", "dnn_dnn_spcen_512_256.jit")
     return DNNDNNModelWrapper(model_path)
